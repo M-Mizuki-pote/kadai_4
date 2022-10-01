7df5f2e942ab6d1debc9678ccda8e09d7d200400
EC2 Default User <ec2-user@ip-172-31-8-150.us-west-1.compute.internal>
2022-10-01 12:40:34 +0000

    課題4再提出

--- memo.rb
+++ memo.rb
@@ -1,27 +1,33 @@
- #課題の確認お願いいたします
- 
  require "csv"
- 
  puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
- 
  memo_type = gets.to_i
  
  if memo_type==1
-   puts "メモを作成します。内容を記入してください。"
-   puts "エンターキーを押したあと、Ctrl+Dで保存します。"
-   content=STDIN.read
-   memo=content.chomp
-   CSV.open('file.csv', 'w') do |csv|
-     csv.puts ["#{memo}"]
-   end
+  puts "新規メモを作成します。拡張子を除いたファイル名を入力してください。"
+  file_name = gets.chomp
+   
+  puts "メモの内容を記入してください。"
+  puts "エンターキーを押したあと、Ctrl+Dで保存します。"
+  content=STDIN.read
+  memo=content.chomp
+  
+  CSV.open("#{file_name}", 'w') do |csv|
+   csv.puts ["#{memo}"]
+  end
+   
  elsif memo_type==2
-    puts "メモを編集します。"
-    puts "エンターキーを押したあと、Ctrl+Dで保存します。"
-    date=CSV.read('file.csv')
-     puts date
-    content=STDIN.read
-    memo=content.chomp
-    CSV.open('file.csv', 'a') do |csv|
-     csv.puts ["#{memo}"]
-   end
+  puts "メモを編集します。編集するファイル名を入力してください。（拡張子を除いて入力してください）"
+  file_name = gets.chomp
+    
+  puts "追加する内容を記入してください。"
+  puts "エンターキーを押したあと、Ctrl+Dで保存します。"
+  date=CSV.read("#{file_name}")
+  puts date
+  
+  content=STDIN.read
+  memo=content.chomp
+  
+  CSV.open("#{file_name}", 'a') do |csv|
+   csv.puts ["#{memo}"]
+  end
  end
