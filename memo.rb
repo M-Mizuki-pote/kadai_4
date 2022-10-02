2beba5db5055007639fbff656555048b19a983ec
EC2 Default User <ec2-user@ip-172-31-8-150.us-west-1.compute.internal>
2022-10-02 09:55:48 +0000

    課題4再提出

--- memo.rb
+++ memo.rb
@@ -1,33 +1,27 @@
- require "csv"
- puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
- memo_type = gets.to_i
+require "csv"
+puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+memo_type = gets.to_i
  
- if memo_type==1
+if memo_type==1
   puts "新規メモを作成します。拡張子を除いたファイル名を入力してください。"
   file_name = gets.chomp
-   
   puts "メモの内容を記入してください。"
   puts "エンターキーを押したあと、Ctrl+Dで保存します。"
   content=STDIN.read
   memo=content.chomp
-  
-  CSV.open("#{file_name}", 'w') do |csv|
+  CSV.open("#{file_name}.csv", "w") do |csv|
    csv.puts ["#{memo}"]
   end
-   
- elsif memo_type==2
+elsif memo_type==2
   puts "メモを編集します。編集するファイル名を入力してください。（拡張子を除いて入力してください）"
   file_name = gets.chomp
-    
   puts "追加する内容を記入してください。"
   puts "エンターキーを押したあと、Ctrl+Dで保存します。"
-  date=CSV.read("#{file_name}")
+  date=CSV.read("#{file_name}.csv")
   puts date
-  
   content=STDIN.read
   memo=content.chomp
-  
-  CSV.open("#{file_name}", 'a') do |csv|
+  CSV.open("#{file_name}.csv", "a") do |csv|
    csv.puts ["#{memo}"]
   end
- end
+end
